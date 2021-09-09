bca50e13aaa2e3948cfaa3845d0aa8d491b21972
Riki420 <girottiriccardo1998@gmail.com>
2021-09-09 14:31:28 +0200

    add component Header + bgcolor + logo

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+<!--HEADER-->
+    <header class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="container">
+                    <div class="row py-4 bg-head">
+                        <div class="col-1 text-white">
+                            <figure>
+                                <img src="../assets/images/logo.png" alt="logo-spotify" class="img-fluid">
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+    header{
+        height: 100px;
+        background-color: #2e3a46;;
+    }
+    
+</style>
